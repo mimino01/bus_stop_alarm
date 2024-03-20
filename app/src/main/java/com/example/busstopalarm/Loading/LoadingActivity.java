c8875020f6f688693a46d352d21a9be238acf32d
DESKTOP-IDUVUTT\손제훈 <mimino0144@gmail.com>
2024-03-20 09:23:46 +0900

    프론트 기본환경 추가 및 백엔드 (서버겸용) 환경설정

---
+++ app/src/main/java/com/example/busstopalarm/Loading/LoadingActivity.java
@@ -0,0 +1,17 @@
+package com.example.busstopalarm.Loading;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.busstopalarm.R;
+
+public class LoadingActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading);
+		
+//		Intent intent =
+	}
+}
