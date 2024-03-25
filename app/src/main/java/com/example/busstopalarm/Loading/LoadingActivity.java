78263eae70b26ff9a8e319919f7bab69b7e9c35c
DESKTOP-IDUVUTT\손제훈 <mimino0144@gmail.com>
2024-03-25 13:59:39 +0900

    서버통신 구성중

--- app/src/main/java/com/example/busstopalarm/Loading/LoadingActivity.java
+++ app/src/main/java/com/example/busstopalarm/Loading/LoadingActivity.java
@@ -3,7 +3,16 @@ package com.example.busstopalarm.Loading;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.busstopalarm.Login.LoginActivity;
 import com.example.busstopalarm.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class LoadingActivity extends AppCompatActivity {
 	
@@ -12,6 +21,38 @@ public class LoadingActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_loading);
 		
-//		Intent intent =
+		
+		try {
+			Socket socket = new Socket("172.20.10.7",8000);
+			
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+			
+			String outputMessage = "서버야 넣을께";
+			out.write(outputMessage.getBytes());
+			out.flush();
+			
+			byte[] inputData = new byte[100];
+			int length = in.read(inputData);
+			String inputMessage = new String(inputData, 0, length);
+			
+			socket.close();
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+		Button button = findViewById(R.id.next);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//화면전환
+				Intent intent = new Intent(LoadingActivity.this, LoginActivity.class);
+				startActivity(intent);
+				
+				Toast.makeText(getApplicationContext(), "화면 전환 성공", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
--- server/bus_stop_alarm_server/src/Main.java
+++ server/bus_stop_alarm_server/src/Main.java
@@ -1,5 +1,36 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(8000);
+			Socket socket = serverSocket.accept();
+			
+			System.out.println("서버on");
+			
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+			
+			byte[] inputData = new byte[100];
+			int length = in.read(inputData);
+			String inputMessage = new String(inputData, 0, length);
+			
+			System.out.println("클라이언트에서 받은 데이터 : " + inputMessage);
+			
+			String outputMessage = "나다 서버";
+			out.write(outputMessage.getBytes());
+			out.flush();
+			
+			socket.close();
+			serverSocket.close();
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
